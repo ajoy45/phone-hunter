fd46edc2dad71d772cb4a7e680b6c71b5be1296b
ajoy45 <ajoypust@gmail.com>
2023-02-26 10:11:10 +0600

    phone added

--- js/phone.js
+++ js/phone.js
@@ -7,12 +7,14 @@ const phoneLoad=async()=>{
 const showPhone=(phones)=>{
     const cardContainer=document.getElementById('card-container');
      phones.forEach(phone=>{
+           const{phone_name,image}=phone
          const div=document.createElement('div');
          div.classList.add('card');
+        
          div.innerHTML=`
-         <img src="..." class="card-img-top" alt="...">
-         <div class="card-body">
-           <h5 class="card-title">Card title</h5>
+         <img src="${image}" class="card-img-top p-4" alt="...">
+         <div class="card-body ">
+           <h5 class="card-title">${phone_name}</h5>
            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
            <a href="#" class="btn btn-primary">Go somewhere</a>
          </div>
